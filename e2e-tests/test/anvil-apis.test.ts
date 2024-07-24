7b8392bb2384c8b9da5cf6679c3d03e8e2630a91
Vaclav Barta <vbarta@mangrove.cz>
2024-07-24 09:21:02 +0200

    added anvil_mine test

--- e2e-tests/test/anvil-apis.test.ts
+++ e2e-tests/test/anvil-apis.test.ts
@@ -34,3 +34,27 @@ describe("anvil_setNonce", function () {
     expect(nonce).to.equal(newNonce);
   });
 });
+
+describe("anvil_mine", function () {
+  it("Should mine multiple blocks with a given interval", async function () {
+    // Arrange
+    const numberOfBlocks = 100;
+    const intervalInSeconds = 60;
+    const startingBlock = await provider.getBlock("latest");
+    const startingTimestamp: number = await provider.send("config_getCurrentTimestamp", []);
+
+    // Act
+    await provider.send("anvil_mine", [
+      ethers.utils.hexlify(numberOfBlocks),
+      ethers.utils.hexlify(intervalInSeconds),
+    ]);
+
+    // Assert
+    const latestBlock = await provider.getBlock("latest");
+    expect(latestBlock.number).to.equal(startingBlock.number + numberOfBlocks, "Block number mismatch");
+    expect(latestBlock.timestamp).to.equal(
+      startingTimestamp + (numberOfBlocks - 1) * intervalInSeconds * 1000 + 1,
+      "Timestamp mismatch"
+    );
+  });
+});

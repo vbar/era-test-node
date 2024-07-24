037399381db666b5e28c4f2b46382b077a2045a3
Vaclav Barta <vbarta@mangrove.cz>
2024-07-24 14:03:44 +0200

    added anvil_impersonateAccount & anvil_stopImpersonatingAccount test

--- e2e-tests/test/anvil-apis.test.ts
+++ e2e-tests/test/anvil-apis.test.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { Wallet } from "zksync-web3";
+import { RichAccounts } from "../helpers/constants";
 import { getTestProvider } from "../helpers/utils";
 import { ethers } from "hardhat";
 
@@ -56,5 +57,32 @@ describe("anvil_mine", function () {
       startingTimestamp + (numberOfBlocks - 1) * intervalInSeconds * 1000 + 1,
       "Timestamp mismatch"
     );
+  });
+});
+
+describe("anvil_impersonateAccount & anvil_stopImpersonatingAccount", function () {
+  it("Should allow transfers of funds without knowing the Private Key", async function () {
+    // Arrange
+    const userWallet = Wallet.createRandom().connect(provider);
+    const beforeBalance = await provider.getBalance(RichAccounts[5].Account);
+
+    // Act
+    await provider.send("anvil_impersonateAccount", [RichAccounts[5].Account]);
+
+    const signer = await ethers.getSigner(RichAccounts[5].Account);
+    const tx = {
+      to: userWallet.address,
+      value: ethers.utils.parseEther("0.42"),
+    };
+
+    const recieptTx = await signer.sendTransaction(tx);
+    await recieptTx.wait();
+
+    await provider.send("anvil_stopImpersonatingAccount", [RichAccounts[5].Account]);
+
+    // Assert
+    expect((await userWallet.getBalance()).eq(ethers.utils.parseEther("0.42"))).to.true;
+    expect((await provider.getBalance(RichAccounts[5].Account)).eq(beforeBalance.sub(ethers.utils.parseEther("0.42"))))
+      .to.true;
   });
 });

949a4a8a3a142f191d980565721a9cfdbfb9856f
MexicanAce <nicolasvillanueva@msn.com>
2024-09-26 17:51:18 +0100

    fix lint

--- e2e-tests/test/fib.test.ts
+++ e2e-tests/test/fib.test.ts
@@ -1,4 +1,3 @@
-import { ethers } from "ethers";
 import { Wallet } from "zksync-web3";
 import * as hre from "hardhat";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
@@ -11,14 +10,14 @@ describe("Test Fib error flags", function () {
   it("Should print to the console NOT ENOUGH ERGS", async function () {
     const action = async () => {
       const wallet = new Wallet(RichAccounts[0].PrivateKey, provider);
-  
+
       const deployer = new Deployer(hre, wallet);
       const fib = await deployContract(deployer, "Fib");
-      const n = await fib.fib(100);
+      await fib.fib(100);
     };
 
     // This is expected to throw and the console is expected to show:
-    //   XX:YY:ZZ ERROR !! Got error flags: 
+    //   XX:YY:ZZ ERROR !! Got error flags:
     //   XX:YY:ZZ ERROR NOT ENOUGH ERGS
     await expectThrowsAsync(action, "call revert exception");
   });
